e157a9195e068830f5573b69ee555dd88ca09793
hailiechoi <ec3135@columbia.edu>
2015-10-13 15:56:21 -0400

    eidted belongs_to :products

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User
   field :email, type: String
   field :password, type: String
   field :gender, type: String
-has_many:products
+has_many :products
+
+  validates :name, :presence => true
+  validates :gender, :presence => true
+
 end
